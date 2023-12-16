cf14cef8b7b63bf4bfb87b025230ac9439c0d2b7
Austin Wise <AustinWise@gmail.com>
2023-12-16 15:04:43 -0800

    Sort usings in gctoclreventsink.cpp

--- src/coreclr/nativeaot/Runtime/gctoclreventsink.cpp
+++ src/coreclr/nativeaot/Runtime/gctoclreventsink.cpp
@@ -1,8 +1,11 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#include "common.h"
+// This is included to avoid redeclarations in headers that check for __GCENV_BASE_INCLUDED__
+#include "gcenv.h"
+
 #include "gctoclreventsink.h"
+
 #include "thread.h"
 
 GCToCLREventSink g_gcToClrEventSink;
