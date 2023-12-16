d7d4124ba66b63f13b7fd5126425c9661e4dfc84
Austin Wise <AustinWise@gmail.com>
2023-12-16 15:15:32 -0800

    Reduce includes in MethodTable.cpp

--- src/coreclr/nativeaot/Runtime/MethodTable.cpp
+++ src/coreclr/nativeaot/Runtime/MethodTable.cpp
@@ -1,14 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#include "common.h"
-#include "CommonTypes.h"
-#include "CommonMacros.h"
-#include "daccess.h"
-#include "rhassert.h"
-#include "rhbinder.h"
+
 #include "MethodTable.h"
-#include "PalRedhawkCommon.h"
-#include "PalRedhawk.h"
+
+#include "CommonMacros.h"
 
 #pragma warning(disable:4127) // C4127: conditional expression is constant
 
