d6036b079b72673e0372c7cbb9560d26e4e9c509
Austin Wise <AustinWise@gmail.com>
2023-12-16 16:04:16 -0800

    Reduce includes in startup.cpp

--- src/coreclr/nativeaot/Runtime/startup.cpp
+++ src/coreclr/nativeaot/Runtime/startup.cpp
@@ -1,28 +1,17 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#include "common.h"
-#include "CommonTypes.h"
-#include "CommonMacros.h"
-#include "daccess.h"
-#include "PalRedhawkCommon.h"
-#include "PalRedhawk.h"
-#include "rhassert.h"
-#include "slist.h"
-#include "varint.h"
-#include "regdisplay.h"
-#include "StackFrameIterator.h"
-#include "thread.h"
-#include "holder.h"
-#include "Crst.h"
-#include "event.h"
-#include "threadstore.h"
-#include "RuntimeInstance.h"
-#include "rhbinder.h"
+
 #include "CachedInterfaceDispatch.h"
-#include "RhConfig.h"
-#include "stressLog.h"
+#include "CommonMacros.h"
+#include "PalRedhawk.h"
 #include "RestrictedCallouts.h"
+#include "RhConfig.h"
+#include "RuntimeInstance.h"
+#include "stressLog.h"
+#include "thread.h"
+#include "threadstore.h"
 #include "yieldprocessornormalized.h"
+
 #include <minipal/cpufeatures.h>
 
 #ifdef FEATURE_PERFTRACING
