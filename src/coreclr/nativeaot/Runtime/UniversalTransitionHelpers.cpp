d8a8fb922df91bfec3946c64e7182a5e9579d0ed
Austin Wise <AustinWise@gmail.com>
2023-12-16 16:32:13 -0800

    Reduce includes in UniversalTransitionHelpers.cpp

--- src/coreclr/nativeaot/Runtime/UniversalTransitionHelpers.cpp
+++ src/coreclr/nativeaot/Runtime/UniversalTransitionHelpers.cpp
@@ -1,9 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#include "common.h"
-#include "CommonTypes.h"
+
+#include <cstdint>
+
 #include "CommonMacros.h"
-#include "PalRedhawkCommon.h"
 #include "PalRedhawk.h"
 
 #ifdef _DEBUG
