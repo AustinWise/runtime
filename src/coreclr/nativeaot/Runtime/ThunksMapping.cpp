e29183aa9800855c8a916724946f38c806184c36
Austin Wise <AustinWise@gmail.com>
2023-12-16 16:29:15 -0800

    Reduce includes in ThunksMapping.cpp

--- src/coreclr/nativeaot/Runtime/ThunksMapping.cpp
+++ src/coreclr/nativeaot/Runtime/ThunksMapping.cpp
@@ -1,12 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-#include "common.h"
 
-#include "CommonTypes.h"
+#include <cstdint>
+
 #include "CommonMacros.h"
-#include "daccess.h"
-#include "PalRedhawkCommon.h"
-#include "volatile.h"
 #include "PalRedhawk.h"
 #include "rhassert.h"
 
